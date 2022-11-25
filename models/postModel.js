5c74f199f323afa90cac98822c5d75041449d0d7
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-25 11:32:52 -0800

    add comments property

--- models/postModel.js
+++ models/postModel.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
     },
     body: {
         type: String,
-        required: true
     },
     timestamp: {
         type: Date,
-        required: true
     },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
     isPublished: {
         type: Boolean,
-        required: true
     }
 })
 
