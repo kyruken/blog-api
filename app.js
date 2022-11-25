25ad00ddd5f1e3dcb594329ebe93266db736954f
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-25 11:32:41 -0800

    add db connection

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 const blogRouter = require('./routes/blog')
 
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', error => {console.error(error)})
+
+app.use(express.urlencoded({extended: true}));
 
 app.use('/posts', blogRouter)
 
