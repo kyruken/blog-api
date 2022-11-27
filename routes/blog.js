1f8569abd79470919f5976d57e606d3dec7f58b9
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-27 11:49:40 -0800

    add jwt verify token authorization to routes

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const async = require('async');
+const jwt = require('jsonwebtoken');
 const Post = require('../models/postModel')
 const Comment = require('../models/commentModel');
 
+
 const router = express.Router();
 
-//Posts
+//Blogs
 router.get('/', (req, res, next) => {
     Post.where('title')
     .exec((err, allPosts) => {
@@ -30,63 +32,83 @@ router.get('/:postId', (req, res, next) => {
     })
 })
 
-router.post('/', (req, res, next) => {
-    const newPost = new Post({
-        title: req.body.title,
-        body: req.body.description,
-        timestamp: new Date(),
-        comments: [],
-        isPublished: false
-    })
-
-    newPost.save((err) => {
+router.post('/', verifyToken, (req, res, next) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
         if (err) {
-            res.sendStatus(500);
-            return next(err);
+            res.send(err);
+            res.sendStatus(403);
+        } else {
+            const newPost = new Post({
+                title: req.body.title,
+                body: req.body.description,
+                timestamp: new Date(),
+                comments: [],
+                isPublished: false
+            })
+        
+            newPost.save((err) => {
+                if (err) {
+                    res.sendStatus(500);
+                    return next(err);
+                }
+        
+                res.sendStatus(200);
+            });
         }
-
-        res.sendStatus(200);
-    });
+    })
 })
 
-router.put('/:postId', (req, res, next) => {
+router.put('/:postId', verifyToken, (req, res, next) => {
 
-    Post.findById(req.params.postId, (err, thePost) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
         if (err) {
-            res.sendStatus(500);
-            return next(err);
+            res.sendStatus(403);
         }
-
-        const newPost = new Post({
-            _id: req.params.postId,
-            title: req.body.title,
-            body: req.body.description,
-            timestamp: new Date(),
-            comments: typeof thePost.comments === "undefined" ? [] : thePost.comments,
-            //When we connect a frontend, we need to change isPublished
-            //to take in a req.body.isPublished 
-            isPublished: thePost.isPublished
-        })
-
-        Post.findByIdAndUpdate(req.params.postId, newPost, (err) => {
+    
+        Post.findById(req.params.postId, (err, thePost) => {
             if (err) {
                 res.sendStatus(500);
                 return next(err);
             }
     
-            res.sendStatus(200);
-        })
-    });
+            const newPost = new Post({
+                _id: req.params.postId,
+                title: req.body.title,
+                body: req.body.description,
+                timestamp: new Date(),
+                comments: typeof thePost.comments === "undefined" ? [] : thePost.comments,
+                //When we connect a frontend, we need to change isPublished
+                //to take in a req.body.isPublished 
+                isPublished: thePost.isPublished
+            })
+    
+            Post.findByIdAndUpdate(req.params.postId, newPost, (err) => {
+                if (err) {
+                    res.sendStatus(500);
+                    return next(err);
+                }
+        
+                res.sendStatus(200);
+            })
+        });
 
+    })
 })
 
 router.delete('/:postId', (req, res, next) => {
-    Post.findByIdAndDelete(req.params.postId, (err) => {
+
+    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
         if (err) {
-            res.sendStatus(500);
-            return next(err);
+            res.sendStatus(403);
         }
-        res.sendStatus(200);
+
+        Post.findByIdAndDelete(req.params.postId, (err) => {
+            if (err) {
+                res.sendStatus(500);
+                return next(err);
+            }
+            res.sendStatus(200);
+        })
     })
 })
 
@@ -143,28 +165,48 @@ router.get('/:postId/comments/:commentId', (req, res, next) => {
 })
 
 router.delete('/:postId/comments/:commentId', (req, res, next) => {
-    Post.findById(req.params.postId, (err, thePost) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
         if (err) {
-            res.sendStatus(500);
+            res.sendStatus(403);
         }
-        for (let x = 0; x < thePost.comments.length; x++) {
-            if(thePost.comments[x]._id.toString() === req.params.commentId.toString()) {
-                thePost.comments.splice(x, 1);
-                break;
+
+        Post.findById(req.params.postId, (err, thePost) => {
+            if (err) {
+                res.sendStatus(500);
             }
-        }
-        
-        thePost.save();
-    });
-
-    Comment.findByIdAndDelete(req.params.commentId, (err) => {
-        if (err) {
-            res.sendStatus(500);
-            return next(err);
-        }
-        res.sendStatus(200);
-    });
-
+            for (let x = 0; x < thePost.comments.length; x++) {
+                if(thePost.comments[x]._id.toString() === req.params.commentId.toString()) {
+                    thePost.comments.splice(x, 1);
+                    break;
+                }
+            }
+            
+            thePost.save();
+        });
+    
+        Comment.findByIdAndDelete(req.params.commentId, (err) => {
+            if (err) {
+                res.sendStatus(500);
+                return next(err);
+            }
+            res.sendStatus(200);
+        });    
+    })
 })
+
+// Format of Authorization: Bearer <token>
+function verifyToken(req, res, next) {
+    //Get authorization header value
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        //We split into an array, [Bearer, <token>], then choose bearer[1] to get the token
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 module.exports = router;
