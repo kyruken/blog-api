a2a4838d8288ee25345acd82cbb906dd81fd37e8
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-18 10:09:02 -0800

    change origin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5173'
+    origin: '*'
 }))
 
 app.use('/posts', blogRouter)
